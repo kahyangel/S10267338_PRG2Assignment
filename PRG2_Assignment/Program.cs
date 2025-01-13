7c5adbbfa293b5576dd19755062ecd9018c61fc4
wyseLee <wwyseel@gmail.com>
2025-01-13 17:23:26 +0800

    Commit Program.cs edit

--- PRG2_Assignment/Program.cs
+++ PRG2_Assignment/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PRG2_Assignment;
+
+// keep track of number of airlines, boarding gates, and flights
+int airlineCount = 0;
+int boardingGateCount = 0;
+int flightCount = 0;
+
+using (StreamReader sr = new StreamReader("airlines.csv"))
+{
+    // reads the header of flights.csv
+    string? s = sr.ReadLine();
+
+    // reads the contents of flights.csv
+    while ((s = sr.ReadLine()) != null)
+    {
+        airlineCount++;
+    }
+}
+
+using (StreamReader sr = new StreamReader("boardinggates.csv"))
+{
+    // reads the header of flights.csv
+    string? s = sr.ReadLine();
+
+    // reads the contents of flights.csv
+    while ((s = sr.ReadLine()) != null)
+    {
+        boardingGateCount++;
+    }
+}
+
+using (StreamReader sr = new StreamReader("flights.csv"))
+{
+    // reads the header of flights.csv
+    string? s = sr.ReadLine();
+    
+    // reads the contents of flights.csv
+    while ((s = sr.ReadLine()) != null)
+    {
+        flightCount++;
+    }
+}
+
+Console.WriteLine("Loading Airlines..." +
+    $"\n{airlineCount} Airlines Loaded!" +
+    "\nLoading Boarding Gates..." +
+    $"\n{boardingGateCount} Boarding Gates Loaded!" +
+    "\nLoading Flights..." +
+    $"\n{flightCount} Flights Loaded!");
+
+
+
+void DisplayMenu()
+// Displays the menu and options
+{
+    Console.WriteLine("=============================================" +
+        "\nWelcome to Changi Airport Terminal 5" +
+        "\n=============================================" +
+        "\n1. List All Flights" +
+        "\n2. List Boarding Gates" +
+        "\n3. Assign a Boarding Gate to a Flight" +
+        "\n4. Create Flight" +
+        "\n5. Display Airline Flights" +
+        "\n6. Modify Flight Details" +
+        "\n7. Display Flight Schedule" +
+        "\n0. Exit");
+}
